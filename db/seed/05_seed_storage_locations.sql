ea9df5d89105fd6ff2684eeecf9721af02b908e3
Eric Simonds <eric@greenebt.com>
2025-04-15 15:09:02 +0200

    fix(seed): replace double quotations for single quotations on storage_locations INSERT.

--- db/seed/05_seed_storage_locations.sql
+++ db/seed/05_seed_storage_locations.sql
@@ -2,8 +2,8 @@ IF NOT EXISTS(SELECT 1 FROM storage_locations)
 BEGIN
     INSERT INTO storage_locations(location_name, location_type, description)
     VALUES
-        ("2255", "Shelf", NULL),
-        ("2250", "Shelf", NULL),
-        ("A", "Room", NULL),
-        ("Airlock", "Production", NULL);
+        ('2255', 'Shelf', NULL),
+        ('2250', 'Shelf', NULL),
+        ('A', 'Room', NULL),
+        ('Airlock', 'Production', NULL);
 END;
