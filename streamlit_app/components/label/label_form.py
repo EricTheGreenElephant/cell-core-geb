aa74cc81c85e523d2b6e838e4c404eaa689cd6a4
Eric <eric@greenebt.com>
2025-11-21 11:37:46 +0100

    feature: manual label generator when IDs don't match during testing

--- streamlit_app/components/label/label_form.py
+++ streamlit_app/components/label/label_form.py
@@ -5,38 +5,85 @@ from components.label.label_generator import generate_label_with_overlays
 from services.label_services import get_label_data_by_product_id
 from db.orm_session import get_session
 from streamlit.components.v1 import html
+from constants.label_constants import QR_LABEL_DATA_SPECS
 
 
 def render_label_form():
     st.subheader("Generate Product Label")
 
-    product_id = st.text_input("Enter Product ID")
+    product_ids = []
+    # Use for manual label generator
+    manual_entry = st.checkbox(
+        label="Select for manual product ID entry:",
+        value=False,
+        help="If you need to manually generate Product ID for label, click here. This will generally be needed throughout testing, since Product IDs may not match."
+    )
+
+    # product_id = st.text_input("Enter Product ID")
+    product_id_search = st.number_input(
+        label="Enter Product ID",
+        min_value=0,
+        step=1
+    )
+
+    if manual_entry:
+        product_amount = st.number_input(
+            label="Enter number of products for label printing.",
+            min_value=0,
+            step=1,
+            help="This will automatically generate the addtional IDs for label printing."
+        )
+
+        product_sku = st.selectbox(
+            label="Choose the Product Specs",
+            options=QR_LABEL_DATA_SPECS.keys()
+        )
+        product_specs = QR_LABEL_DATA_SPECS[product_sku]
+        product_ids = [product_id_search + x for x in range(0, product_amount)]
+    else:
+        product_ids.append(product_id_search)
+
+
     label_choice = st.selectbox("Choose Label", options=["Select...", "Harvest", "Bag and Package"], index=0)
+
+    product_id = st.radio(label="Choose Product ID", options=product_ids)
+
     generate = st.button("Generate Label", type="primary", use_container_width=True)
     
     if generate and product_id and label_choice != "Select...":
-        try:
-            product_id_int = int(product_id)
-        except ValueError:
-            st.error("Product ID must be a number.")
-            return
+        if manual_entry:
+            today = datetime.today()
+            expire_date = today.replace(year=today.year + 1)
+            expiration_formatted = f"{expire_date.month} / {expire_date.year}"
+            product_type = product_specs["product_type"]
+            surface_area = product_specs["surface_area"]
+        else:
+            try:
+                product_id_int = int(product_id)
+            except ValueError:
+                st.error("Product ID must be a number.")
+                return
 
-        with get_session() as db:
-            data = get_label_data_by_product_id(db, product_id_int)
-        
-        if not data:
-            st.warning("Product not found.")
-            return
+            with get_session() as db:
+                data = get_label_data_by_product_id(db, product_id_int)
+            
+            if not data:
+                st.warning("Product not found.")
+                return
 
-        expiration_raw = data["expiration_date"]  # e.g. "2026-07-15"
-        exp_date = datetime.strptime(expiration_raw, "%Y-%m-%d")
-        expiration_formatted = f"{exp_date.month} / {exp_date.year}"
+            expiration_raw = data["expiration_date"]  # e.g. "2026-07-15"
+            exp_date = datetime.strptime(expiration_raw, "%Y-%m-%d")
+            expiration_formatted = f"{exp_date.month} / {exp_date.year}"
+            product_sku = data["reference_number"]
+            product_type = data["product_type"]
+            surface_area = data["volume"]
+            product_id = data["product_id"]
 
         label_fields = [
-            {"text": data["reference_number"], "position": (380, 40), "font_size": 44},
-            {"text": data["product_type"], "position": (270, 169), "font_size": 42},
-            {"text": f"{data['volume']} cm\u00b2", "position": (150, 246), "font_size": 42},
-            {"text": f"{data['product_id']}", "position": (210, 550), "font_size": 42},
+            {"text": product_sku, "position": (380, 40), "font_size": 44},
+            {"text": product_type, "position": (270, 169), "font_size": 42},
+            {"text": f"{surface_area} cm\u00b2", "position": (150, 246), "font_size": 42},
+            {"text": f"{product_id}", "position": (210, 550), "font_size": 42},
             {"text": expiration_formatted, "position": (210, 660), "font_size": 42},
         ]
     
@@ -58,12 +105,16 @@ def render_label_form():
         else:
             background_path = "streamlit_app/assets/GEB-Label2-Image-wo-Text.png"
 
-        label_img = generate_label_with_overlays(
-            background_path=background_path,
-            fields=label_fields,
-            qr_data=qr_data,
-            qr_position=qr_position
-        )
+
+        for pid in product_ids:
+            label_fields.append({"text": f"{product_id}", "position": (210, 550), "font_size": 42})
+            label_img = generate_label_with_overlays(
+                background_path=background_path,
+                fields=label_fields,
+                qr_data=qr_data,
+                qr_position=qr_position
+            )
+
 
         st.success("Label generated.")
 
---
+++ streamlit_app/constants/label_constants.py
@@ -0,0 +1,28 @@
+# Dataset for manual QR Code Generation
+
+QR_LABEL_DATA_SPECS = {
+    'GEB-CS10KTCS-4': {
+        'product_type': '10K',
+        'surface_area': '10197'
+    },
+    'GEB-CS6KTCS-2': {
+        'product_type': '6K',
+        'surface_area': '6001'
+    },
+    'GEB-CS6KTCS-4': {
+        'product_type': '6K',
+        'surface_area': '6124'
+    },
+    'GEB-CSmTCS': {
+        'product_type': 'CS-Mini',
+        'surface_area': '856'
+    },
+    'GEB-CSmTC2S': {
+        'product_type': 'CS-Mini',
+        'surface_area': '851'
+    },
+    'GEB-CSmTCS-2': {
+        'product_type': 'CS-Mini',
+        'surface_area': '851'
+    }
+}
